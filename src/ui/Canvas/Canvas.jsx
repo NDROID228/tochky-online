30b9a7bfc371b9386530bcdfa48b08660cd4f62e
NDROID228 <djunsonik@gmail.com>
2024-03-13 10:31:37 +0200

    resolved stash commit

--- src/ui/Canvas/Canvas.jsx
+++ src/ui/Canvas/Canvas.jsx
@@ -1,11 +1,5 @@
-<<<<<<< Updated upstream
 import React, { useEffect, useState } from "react";
 import { Stage, Layer, Circle, Rect } from "react-konva";
-=======
-import React, { useEffect, useState, useLayoutEffect } from "react";
-import { Stage, Rect, Layer, Circle, Line } from "react-konva";
-// import getWindowSize from "../../usefulFunctions/getWindowSize";
->>>>>>> Stashed changes
 
 const Canvass = () => {
   const [stagePos, setStagePos] = useState({ x: 2500, y: 2500 });
@@ -50,7 +44,6 @@ const Canvass = () => {
   }, [keyPressed]);
 
   return (
-<<<<<<< Updated upstream
     <div
       style={{
         width: "100vw",
@@ -89,29 +82,6 @@ const Canvass = () => {
           <Rect x={0} y={0} width={5000} height={5000} />
           {/* Circle - это кружочек, который видно на экране со старта для теста движения Layer */}
           <Circle x={0} y={0} stroke="black" radius={20} fill="black" />
-=======
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Stage width={windowSize.width} height={windowSize.height}>
-        {/* DO NOT UNCOMMENT THIS SHIT!!1! */}
-        {/* <Layer x={windowSize.width / 2} y={windowSize.height / 2}>
-          <Circle x={200} y={200} stroke="black" radius={20} />
-          <Circle x={240} y={200} stroke="black" radius={20} />
-          <Line points={[200, 180, 200, 100]} stroke={"black"} />
-          <Line points={[240, 180, 240, 100]} stroke={"black"} />
-          <Circle x={220} y={100} stroke="black" radius={20} />
-          <Line points={[220, 80, 220, 100]} stroke={"black"} />
-        </Layer> */}
-        {/* <Layer x={windowSize.width / 2} y={windowSize.height / 2}>
-          <Circle x={20} y={0} stroke="black" radius={20} />
-          <Circle x={-20} y={0} stroke="black" radius={20} />
-          <Line points={[-20, 20, -20, 80]} stroke={"black"} />
-          <Line points={[20, 20, 20, 80]} stroke={"black"} />
-          <Circle stroke="black" radius={20} />
-          <Line stroke={"black"} />
-        </Layer> */}
-        <Layer>
-          <Rect x={-2500} y={-2500} fill="#228a96" width={5000} height={5000} />
->>>>>>> Stashed changes
         </Layer>
       </Stage>
     </div>
